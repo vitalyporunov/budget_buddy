cdfb9445ee2a77a8877c96c6699eff7efd62d661
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2025-03-23 16:59:33 +0000

    added code for dashboards tests

--- budgetbuddy/dashboard/tests.py
+++ budgetbuddy/dashboard/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+
+class DashboardViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpass123')
+        self.dashboard_url = reverse('dashboard')  
+
+    def test_dashboard_requires_login(self):
+        response = self.client.get(self.dashboard_url)
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(reverse('login'), response.url)
+
+    def test_dashboard_accessible_with_login(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(self.dashboard_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'dashboard/dashboard.html')
+
+    def test_dashboard_contains_user_content(self):
+        self.client.login(username='testuser', password='testpass123')
+        response = self.client.get(self.dashboard_url)
+        self.assertContains(response, 'Welcome')  
+        self.assertContains(response, self.user.username)
