a6e5ea4d714add26757fe88c531224a6d2704676
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2025-04-12 18:52:20 +0100

    changes to redner.yaml

--- budgetbuddy/budgetbuddy/wsgi.py
+++ budgetbuddy/budgetbuddy/wsgi.py
@@ -6,10 +6,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-import sys
-print("PYTHON PATH:", sys.path)
-print("FILES:", os.listdir('.'))
-
 import os
 from django.core.wsgi import get_wsgi_application
 
