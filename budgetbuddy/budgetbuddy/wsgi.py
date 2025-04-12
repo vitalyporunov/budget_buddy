4bd57d4225a164600162a32a8d0aa815e6fe4aa5
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2025-04-12 18:38:04 +0100

    changes to wsgi.py

--- budgetbuddy/budgetbuddy/wsgi.py
+++ budgetbuddy/budgetbuddy/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
+# Set the settings module for the 'budgetbuddy' Django project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budgetbuddy.settings')
 
+# Get the WSGI application
 application = get_wsgi_application()
+
 
