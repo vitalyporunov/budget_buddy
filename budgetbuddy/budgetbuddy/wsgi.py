0fee24376ea6f462c92b8823217bb416c7c9079b
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2025-04-12 18:47:16 +0100

    changes to wsgi.py

--- budgetbuddy/budgetbuddy/wsgi.py
+++ budgetbuddy/budgetbuddy/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
+import sys
+print("PYTHON PATH:", sys.path)
+print("FILES:", os.listdir('.'))
 
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Set the settings module for the 'budgetbuddy' Django project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budgetbuddy.settings')
 
 # Get the WSGI application
