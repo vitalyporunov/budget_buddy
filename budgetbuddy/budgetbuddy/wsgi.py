857ad2c75967a36412dfff100d2c0b4b53cb4239
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2025-04-12 19:08:48 +0100

    new render.yaml

--- budgetbuddy/budgetbuddy/wsgi.py
+++ budgetbuddy/budgetbuddy/wsgi.py
@@ -11,7 +11,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budgetbuddy.settings')
 
-# Get the WSGI application
 application = get_wsgi_application()
+
 
 
