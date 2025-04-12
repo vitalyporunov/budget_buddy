765892aaedb93077321aa451ee3a8bac75a9f956
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2025-04-12 19:30:41 +0100

    debug wsgi.py

--- budgetbuddy/budgetbuddy/wsgi.py
+++ budgetbuddy/budgetbuddy/wsgi.py
@@ -6,6 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
+import os, sys
+print("DEBUG:: PATH", sys.path)
+print("DEBUG:: FILES", os.listdir("."))
+
 import os
 from django.core.wsgi import get_wsgi_application
 
